b30a2b671de447a9f7df6254cd955879a8f27733
DmitreyZl <70319100+DmitreyZl@users.noreply.github.com>
2024-08-21 23:49:39 +0300

    Update ad_insights.py

--- tap_facebook/streams/ad_insights.py
+++ tap_facebook/streams/ad_insights.py
@@ -213,7 +213,7 @@ class AdsInsightStream(Stream):
         else:
             #upload history
             config_start_date = pendulum.today().date()
-            config_start_date = config_start_date.subtract(days=233)
+            config_start_date = config_start_date.subtract(days=1)#234)
             incremental_start_date = config_start_date
         
         lookback_start_date = incremental_start_date.subtract(days=lookback_window)
